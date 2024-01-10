17e46e31ec4c1904f03c5ea24ba3f7130f94e95c
Tim Tandiyo <tim@timtandiyo.codes>
2024-01-09 16:14:15 -0800

    Add title templating

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
 	import '../app.css';
+	import { page } from '$app/stores';
 	import NavBar from '$lib/components/navbar.svelte';
 </script>
 
+<svelte:head>
+	{#if $page.data.title}
+		<title>
+			{$page.data.title} – timtandiyo.codes
+		</title>
+	{:else}
+		<title>timtandiyo.codes</title>
+	{/if}
+</svelte:head>
+
 <NavBar />
+
 <slot />
