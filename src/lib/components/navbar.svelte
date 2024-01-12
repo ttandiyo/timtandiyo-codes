d85bd16203e6cfab33bd425adf6710a5a608ec26
Tim Tandiyo <tim@timtandiyo.codes>
2024-01-11 16:52:42 -0800

    Add burger/close, conditionals

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
   import Logo from '$lib/components/logo.svelte';
+  import CloseIcon from '$lib/icons/close-icon.svelte';
+  import BurgerIcon from '$lib/icons/burger-icon.svelte';
+
   let navOpen = false;
 </script>
 
 <!-- ${settings.stickyNavBar && 'sticky'}  -->
 <div
-  class="sticky z-20 top-0 w-full backdrop-blur-lg bg-cream/80 select-nonept-[env(safe-area-inset-top)] pr-[env(safe-area-inset-right)] pl-[env(safe-area-inset-left)]"
+  class="sticky z-20 top-0 w-full backdrop-blur-lg bg-white/80 select-none pt-[env(safe-area-inset-top)] pr-[env(safe-area-inset-right)] pl-[env(safe-area-inset-left)]"
 >
   <header class="mx-auto max-w-screen-xl flex items-center justify-between border-b border-gray/25">
     <Logo />
@@ -18,16 +21,20 @@
       class="flex justify-center items-center sm:hidden p-2 z-1"
       on:click={() => (navOpen = !navOpen)}
     >
-      <!-- {navOpen ? <CloseIcon /> : <BurgerIcon />} -->
+      {#if navOpen}
+        <CloseIcon />
+      {:else}
+        <BurgerIcon />
+      {/if}
     </button>
   </header>
-  <nav
-    class={`${
-      !navOpen && 'hidden'
-    } flex sm:hidden mx-auto max-w-screen-xl items-center justify-between border-b border-gray/25`}
-  >
-    <ul class="flex justify-center items-center gap-4 p-2 max-w-screen-xl flex-wrap mx-auto">
-      <!-- <NavList links={settings.navBarLinks} handleClick={() => setNavOpen(!navOpen)} /> -->
-    </ul>
-  </nav>
+  {#if navOpen}
+    <nav
+      class="flex sm:hidden mx-auto max-w-screen-xl items-center justify-between border-b border-gray/25"
+    >
+      <ul class="flex justify-center items-center gap-4 p-2 max-w-screen-xl flex-wrap mx-auto">
+        <!-- <NavList links={settings.navBarLinks} handleClick={() => setNavOpen(!navOpen)} /> -->
+      </ul>
+    </nav>
+  {/if}
 </div>
