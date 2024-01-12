289c8359118b7d0116a35ccf4ffbd6634f9a1e51
Tim Tandiyo <tim@timtandiyo.codes>
2024-01-11 16:38:20 -0800

    Navbar logo

---
+++ src/lib/components/logo.svelte
@@ -0,0 +1,10 @@
+<h1 class="flex items-center">
+  <a
+    href="/"
+    class="p-2 lg:p-4 text-2xl lg:text-3xl text-transparent bg-clip-text logo-gradient font-extrabold font-mono"
+  >
+    tim tandiyo<span class="font-semibold inline-block">.codes</span><span
+      class="invisible inline-block h-5 lg:h-7 w-1 logo-gradient motion-safe:animate-blink"
+    />
+  </a>
+</h1>
--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
+  import Logo from '$lib/components/logo.svelte';
   let navOpen = false;
 </script>
 
 <!-- ${settings.stickyNavBar && 'sticky'}  -->
 <div
-  class="z-20 top-0 w-full backdrop-blur-lg bg-cream/80 select-nonept-[env(safe-area-inset-top)] pr-[env(safe-area-inset-right)] pl-[env(safe-area-inset-left)]"
+  class="sticky z-20 top-0 w-full backdrop-blur-lg bg-cream/80 select-nonept-[env(safe-area-inset-top)] pr-[env(safe-area-inset-right)] pl-[env(safe-area-inset-left)]"
 >
   <header class="mx-auto max-w-screen-xl flex items-center justify-between border-b border-gray/25">
-    <!-- <Logo /> -->
+    <Logo />
     <nav class="hidden sm:flex">
       <ul class="flex justify-center items-center gap-8 m-2 lg:m-4 flex-wrap">
         <!-- <NavList links={settings.navBarLinks} /> -->
