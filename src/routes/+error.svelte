60db42445bb89a57e462e207eba9985c2e43a636
Tim Tandiyo <tim@timtandiyo.codes>
2024-01-11 18:41:38 -0800

    Add styled 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Content from '$lib/components/content.svelte';
+</script>
+
+<Content>
+  <div class="prose prose-neutral mx-auto lg:prose-lg text-center">
+    <h1 class="lowercase">Uh-oh!</h1>
+    <p>{`The page you're looking for isn't here.`}</p>
+  </div>
+</Content>
