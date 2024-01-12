958ca8bdf119ea51b0242cc8abe671936f5a02d7
Tim Tandiyo <tim@timtandiyo.codes>
2024-01-11 18:27:15 -0800

    Add text helpers

---
+++ src/lib/components/prose.svelte
@@ -0,0 +1,5 @@
+<div
+  class="prose prose-neutral prose-h1:lowercase prose-h2:lowercase prose-h3:lowercase prose-a:lowercase lg:prose-lg break-inside-avoid pb-8 lg:pb-12"
+>
+  <slot />
+</div>
---
+++ src/lib/components/text2columns.svelte
@@ -0,0 +1,1 @@
+<div class="lg:columns-2 lg:gap-24"><slot /></div>
