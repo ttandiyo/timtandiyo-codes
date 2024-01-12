559f64a64cc5139e9ae326dab2f047216e6865e7
Tim Tandiyo <tim@timtandiyo.codes>
2024-01-11 18:41:22 -0800

    Refactor content layout into component

---
+++ src/lib/components/content.svelte
@@ -0,0 +1,5 @@
+<div class="pl-[env(safe-area-inset-left,20px)] pr-[env(safe-area-inset-right)]">
+  <div class="p-2 sm:p-4 lg:p-8 mx-auto max-w-screen-xl">
+    <slot />
+  </div>
+</div>
--- src/routes/(svex)/+layout.svelte
+++ src/routes/(svex)/+layout.svelte
@@ -1,5 +1,7 @@
-<div class="pl-[env(safe-area-inset-left,20px)] pr-[env(safe-area-inset-right)]">
-  <div class="p-2 sm:p-4 lg:p-8 mx-auto max-w-screen-xl">
-    <slot />
-  </div>
-</div>
+<script lang="ts">
+  import Content from '$lib/components/content.svelte';
+</script>
+
+<Content>
+  <slot />
+</Content>
