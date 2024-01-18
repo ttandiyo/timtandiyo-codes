0640ad2b646100c7593b8e0b3230986a4d08d168
Tim Tandiyo <tim@timtandiyo.codes>
2024-01-18 15:33:15 -0800

    Full screen background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,4 +16,18 @@
 
 <NavBar />
 
+<div class="bg-black w-full h-full overflow-clip flex fixed z-[-1]">
+  <div class="z-[-2] [perspective:2000px] overflow-clip w-full h-full absolute opacity-25">
+    <div
+      class="bg-[url('$lib/assets/architect.svg')] overflow-clip opacity-90 bg-[size:100px] bg-center bg-repeat h-[max((150vw,150vh,1414px)] lg:h-[max(250vw,250vh,1768px)] w-[max(150vw,150vh,1414px)] lg:w-[max(150vw,150vh,1768px)] absolute left-[-5000px] right-[-5000px] top-[-5000px] bottom-[-5000px] [transform:rotateZ(37deg)] m-auto motion-safe:animate-slide1"
+    ></div>
+    <div
+      class="bg-[url('$lib/assets/architect.svg')] overflow-clip opacity-60 bg-[size:141px] bg-center bg-repeat h-[max(150vw,150vh,1414px)] lg:h-[max(250vw,250vh,1768px)] w-[max(150vw,150vh,1414px)] lg:w-[max(150vw,150vh,1768px)] absolute left-[-5000px] right-[-5000px] top-[-5000px] bottom-[-5000px] [transform:rotateZ(30deg)] m-auto motion-safe:animate-slide2"
+    ></div>
+    <div
+      class="bg-[url('$lib/assets/architect.svg')] overflow-clip opacity-75 bg-[size:120px] bg-center bg-repeat h-[max((150vw,150vh,1414px)] lg:h-[max(250vw,250vh,1768px)] w-[max(150vw,150vh,1414px)] lg:w-[max(150vw,150vh,1768px)] absolute left-[-5000px] right-[-5000px] top-[-5000px] bottom-[-5000px] [transform:rotateZ(45deg)] m-auto motion-safe:animate-slide3"
+    ></div>
+  </div>
+</div>
+
 <slot />
