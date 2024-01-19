97f5eb7c69dacdf8865054bc632df96dd001dcd4
Tim Tandiyo <tim@timtandiyo.codes>
2024-01-18 16:26:26 -0800

    Enable postcss plugs

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,7 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    '@csstools/postcss-oklab-function': {},
+    '@csstools/postcss-gradients-interpolation-method': {},
   },
 };
